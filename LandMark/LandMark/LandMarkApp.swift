c8e8d72f74dd03b972cb31dbfad907ab19215d33
Jawad Bhatti <jawad.bhatti@ssloman.com>
2024-01-02 22:27:25 +0400

    Created XCode Project

---
+++ LandMark/LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by developer on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
