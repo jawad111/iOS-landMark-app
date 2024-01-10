1869d0496f6abd8539d141962a8637480421efa2
Jawad Bhatti <jawad-hussain@outlook.com>
2024-01-10 23:33:49 +0400

    Updated Row for View

--- LandMark/LandMark/Views/LandMarkRow.swift
+++ LandMark/LandMark/Views/LandMarkRow.swift
@@ -7,16 +7,22 @@
 
 import SwiftUI
 
-
 struct LandmarkRow: View {
-    var landmark: LandMark
+var landmark: Landmark
 
 
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+
+            Spacer()
+        }
     }
 }
-
 
 #Preview {
     LandmarkRow(landmark: landmarks[0])
