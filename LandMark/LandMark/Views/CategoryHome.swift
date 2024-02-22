182b8b5a30fdd6085fda7c8ee6e44d5b477545f9
Jawad Bhatti <jawad-hussain@outlook.com>
2024-02-22 23:50:46 +0400

    Added Navigation Split View

--- LandMark/LandMark/Views/CategoryHome.swift
+++ LandMark/LandMark/Views/CategoryHome.swift
@@ -10,10 +10,14 @@ import SwiftUI
 
 struct CategoryHome: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationSplitView {
+            Text("Split")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
     }
 }
-
 
 #Preview {
     CategoryHome()
